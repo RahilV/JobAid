32bdda0648f85599a90b793ea85791d2738a80f4
Rahil Vithalani <rvithalani.rjv@gmail.com>
2025-02-26 13:02:35 +0530

    new page navbar 1 file

--- footer.js
+++ footer.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error loading footer:", error));
 });
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+});
+
